d8052c747aa39a54933229961b281e0f3f3f26a1
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-17 11:24:26 -0700

    fleshing out keys and fetch js

---
+++ fetch-utils.js
@@ -0,0 +1,27 @@
+// Enter Supabase Information
+const SUPABASE_URL = 'https://xsjxkslcnzexkeuqopsq.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhzanhrc2xjbnpleGtldXFvcHNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIyOTI0MzIsImV4cCI6MTk2Nzg2ODQzMn0.6PW-I5jDnoqhgO6Egpvc5fEpprJ1jHfmAODRlCynp2I';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export function getUser() {
+    return client.auth.session() && client.auth.session().user;
+}
+
+export function fetchPost() {
+    const response = await client.from('post').select('*');
+    console.log(post[0]);
+    return response.data;
+
+}
+
+
+
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    if (response.user) {
+        return response.user;
+    } else {
+        console.error(response.error);
+    }    
+}
