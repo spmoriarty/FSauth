fcb5515005072dd92280d29af5e177836b4b4849
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-18 12:28:36 -0700

    event listener to form

--- auth/auth.js
+++ auth/auth.js
@@ -3,14 +3,14 @@ import { signupUser } from '../fetch-utils.js';
 import { checkAuth } from '../fetch-utils.js';
 
 
-const signUpButton = document.getElementById('sign-up-sub');
+const formSignUp = document.getElementById('sign-up-auth');
 const signInButton = document.getElementById('sign-in-sub');
 
-signUpButton.addEventListener('submit', async (e) => {
+formSignUp.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const data = new FormData(signUpButton);
+    const data = new FormData(formSignUp);
     const user = await signupUser(data.get('email'), data.get('password'));
         if (user) {
             location.replace('/post');
         };
-}
+    }
